f725e609c3fd2e41664263c955e07fcc738c9ee9
Krisprog <74767296+Krisprog@users.noreply.github.com>
2022-06-12 21:53:55 +0300

    Add files via upload

---
+++ precache-manifest.d02d1bad4bf2c74f1584c0d492e41aca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5d57a750f4c9c68b029e87f5e962bfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2976b0b61881304862a7",
+    "url": "/static/css/main.a6fec67e.chunk.css"
+  },
+  {
+    "revision": "62bdcb5ad0028f7e741d",
+    "url": "/static/js/2.6d4190a1.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/static/js/2.6d4190a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2976b0b61881304862a7",
+    "url": "/static/js/main.be54f3e8.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
